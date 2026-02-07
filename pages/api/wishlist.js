70908f1bf72afc86b2a1bb35298e2852e5cbe223
Amel Jridi <amaljridi84@gmail.com>
2026-02-07 09:26:28 +0100

    fix

--- pages/api/wishlist.js
+++ pages/api/wishlist.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
       const wishlist = await Wishlist
         .find({ userEmail })
         .populate("product");
-      
+            console.log("WISHLIST FROM DB 👉", wishlist); // ✅ هنا بالضبط      
       return res.status(200).json(wishlist);
     } catch (error) {
       console.error("Error fetching wishlist:", error);
