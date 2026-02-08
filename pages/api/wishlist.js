22acae428edf5ee2dc517a43e9cd04b76efc0aa1
Amel Jridi <amaljridi84@gmail.com>
2026-02-08 09:18:04 +0100

    public

--- pages/api/wishlist.js
+++ pages/api/wishlist.js
@@ -1,8 +1,8 @@
 import { mongooseConnect } from "@/lib/mongoose";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import { WishedProduct } from "@/models/WishedProduct";
-import { Product } from "@/models/Product";
+import { authOptions } from "./api/auth/[...nextauth]";
+import WishedProduct from "@/models/WishedProduct";
+import  Product  from "@/models/Product";
 
 export default async function handle(req, res) {
   await mongooseConnect();
