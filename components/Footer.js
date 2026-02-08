ed2002409c0ca6f460ceb4035fc4db7f271700bf
Amel Jridi <amaljridi84@gmail.com>
2026-02-08 11:15:56 +0100

    public

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const FooterContainer = styled.footer`
 margin-top: -20px; /* Force le footer à remonter */
   background: #7f7bd1;
-background: radial-gradient(circle at top, rgba(127, 123, 209, 1) 0%, rgba(200, 200, 230, 1) 100%);  width: 100%;
 padding: 40px 20px 40px; /* Réduit de 60px à 40px */  font-family: 'Inter', sans-serif;
   border-top: 1px solid rgba(15, 23, 42, 0.05);
   flex-shrink: 0; 
